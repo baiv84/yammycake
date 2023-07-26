60e678d1a5686435ffbd2f191bda79e5de209fbb
baiv84 <65267292+baiv84@users.noreply.github.com>
2023-07-27 02:02:15 +0700

    Add telegram bot library v13.15

--- yammybot.py
+++ yammybot.py
@@ -1,11 +1,50 @@
-import telegram
+import logging
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from environs import Env
 
 
 env = Env()
 env.read_env()
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 
-if __name__=='__main__':
-    TG_BOT_TOKEN = env('YAMMYCAKE_BOT_TOKEN')
-    print(TG_BOT_TOKEN)
+logger = logging.getLogger(__name__)
+
+
+def start(update: Update, context: CallbackContext):
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()} \- my friend',
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+def help_command(update: Update, context: CallbackContext):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def main():
+    token = env('YAMMYCAKE_BOT_TOKEN')
+    updater = Updater(token)
+
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('help', help_command))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
